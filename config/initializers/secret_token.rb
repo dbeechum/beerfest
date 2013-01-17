0f9fccace56e8ff0bc5c550571aac60b387e4bb0
Drew Beechum <dbeechum@gmail.com>
2013-01-17 15:33:57 -0800

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerfest::Application.config.secret_token = 'b640a130a59503bb5daf82797bb6c096ce5be425b96c5ab34ba2def09dc5ecc4b64021e3643c308c1e8ce58b6be22ef742b5b054badf0458b7807fb0c388c199'
